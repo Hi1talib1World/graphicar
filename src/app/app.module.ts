586b89e3db1699d70dec695748772ed019d883c7
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-29 01:15:32 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderCartComponent } from './layout/components/header-cart/header-cart.component';
 import { HeaderSearchComponent } from './layout/components/header-search/header-search.component';
 import { HeaderHelpDropdownComponent } from './layout/components/header-help-dropdown/header-help-dropdown.component';
+import { CategoryMobileMenuComponent } from './layout/components/category-mobile-menu/category-mobile-menu.component';
+import { CategoriesMenuDropdownComponent } from './layout/components/categories-menu-dropdown/categories-menu-dropdown.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { HeaderHelpDropdownComponent } from './layout/components/header-help-dro
     FooterComponent,
     HeaderCartComponent,
     HeaderSearchComponent,
-    HeaderHelpDropdownComponent
+    HeaderHelpDropdownComponent,
+    CategoryMobileMenuComponent,
+    CategoriesMenuDropdownComponent
   ],
   imports: [
     BrowserModule,
