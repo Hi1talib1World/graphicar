9a1228d9f6f107c01e8e8c20c45270527058c761
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-29 01:15:48 +0100

    Create categories-menu-dropdown.component.ts

---
+++ src/app/layout/components/categories-menu-dropdown/categories-menu-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-menu-dropdown',
+  templateUrl: './categories-menu-dropdown.component.html',
+  styleUrls: ['./categories-menu-dropdown.component.css']
+})
+export class CategoriesMenuDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
