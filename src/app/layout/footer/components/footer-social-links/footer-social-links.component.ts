03a9b6ea169e55854d719152244fedbaddb55c95
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-29 01:23:39 +0100

    Create footer-social-links.component.ts

---
+++ src/app/layout/footer/components/footer-social-links/footer-social-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-social-links',
+  templateUrl: './footer-social-links.component.html',
+  styleUrls: ['./footer-social-links.component.css']
+})
+export class FooterSocialLinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
