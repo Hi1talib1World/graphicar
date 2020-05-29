670bc0b89d6da1e3bcd9c4f5fad263740be69e31
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-29 01:03:33 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
