215435b03d5b04aa2d6117b697fa523d34a0d557
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-29 01:24:05 +0100

    Create footer-contact-info.component.ts

---
+++ src/app/layout/footer/components/footer-contact-info/footer-contact-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-contact-info',
+  templateUrl: './footer-contact-info.component.html',
+  styleUrls: ['./footer-contact-info.component.css']
+})
+export class FooterContactInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
