a605eff64ebb8528b70f48d6409ec42a2b0380ce
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-29 01:16:57 +0100

    Create profile-dropdown.component.ts

---
+++ src/app/layout/components/profile-dropdown/profile-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-dropdown',
+  templateUrl: './profile-dropdown.component.html',
+  styleUrls: ['./profile-dropdown.component.css']
+})
+export class ProfileDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
