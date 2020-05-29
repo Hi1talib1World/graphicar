4e00d1275d9785a21707654fe0153e66eb657310
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-29 01:23:47 +0100

    Create footer-quick-links.component.ts

---
+++ src/app/layout/footer/components/footer-quick-links/footer-quick-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-quick-links',
+  templateUrl: './footer-quick-links.component.html',
+  styleUrls: ['./footer-quick-links.component.css']
+})
+export class FooterQuickLinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
