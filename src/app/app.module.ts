3a6ab74505180b3edc20774456593bd0f6aaba9b
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-29 01:07:39 +0100

    ng g c header cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderCartComponent } from './layout/components/header-cart/header-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderCartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/components/header-cart/header-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-cart',
+  templateUrl: './header-cart.component.html',
+  styleUrls: ['./header-cart.component.css']
+})
+export class HeaderCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
