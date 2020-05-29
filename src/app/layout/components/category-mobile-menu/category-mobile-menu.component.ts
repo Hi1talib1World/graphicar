7fb6c6da90c0d973bdeea8321a5213cbccf8e5f8
Hi1talib1World <hichamoutaleb7@gmail.com>
2020-05-29 01:15:34 +0100

    Create category-mobile-menu.component.ts

---
+++ src/app/layout/components/category-mobile-menu/category-mobile-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-mobile-menu',
+  templateUrl: './category-mobile-menu.component.html',
+  styleUrls: ['./category-mobile-menu.component.css']
+})
+export class CategoryMobileMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
